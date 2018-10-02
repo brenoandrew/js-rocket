90738d5739a192c6419c0c30ae301c0eaa7ab5b8
brenoandrew <37001647+brenoandrew@users.noreply.github.com>
2018-10-02 19:38:37 -0300

    final

--- js/rocket.js
+++ js/rocket.js
@@ -5,16 +5,41 @@ var changeState = function (state) {
     document.body.className = 'body-state' + state;
     clearInterval(timer);
     countdownNumber = 10;
-    document.getElementById('countdown').innerHTML = 
+    document.getElementById('countdown').innerHTML =
             countdownNumber;
 
     //countdown
     if (state == 2) {
         timer = setInterval(function () {
-            countdownNumber = countdownNumber 
+            countdownNumber = countdownNumber
             -1;
-            document.getElementById('countdown').innerHTML = 
+            document.getElementById('countdown').innerHTML =
             countdownNumber;
+
+            if (countdownNumber > 4 && countdownNumber <= 7) {
+              // be nervous
+              document.getElementById('nervous').className =
+              'nervous show';
+
+            } else {
+              document.getElementById('nervous').className =
+              'nervous';
+
+            }
+
+          if (countdownNumber > 1 && countdownNumber <= 4) {
+              // can't wait
+              document.getElementById('cant-wait').className =
+              'cant-wait show';
+
+            } else {
+              document.getElementById('cant-wait').className =
+              'cant-wait';
+
+            };
+
+
+
             if (countdownNumber <= 0) {
                 changeState(3);
 
@@ -25,9 +50,9 @@ var changeState = function (state) {
             var randomNumber = Math.round(Math.random()*10)
 
             console.log('randomNumber:', randomNumber)
-            
+
             // sucess
-            if (randomNumber > 5) {
+            if (randomNumber > 2) {
                 changeState(4);
             } else {
                 changeState(5); // oh no!
